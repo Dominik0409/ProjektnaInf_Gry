0aaab6dde5d0d85610c25d82890ebad5ebabf6ac
rizun1999 <62021379+rizun1999@users.noreply.github.com>
2021-06-09 13:51:09 +0200

    Update warcaby_minimenu.py

--- warcaby_minimenu.py
+++ warcaby_minimenu.py
@@ -1,5 +1,6 @@
 import pygame
-from gra import bialy, czerwony, czarny, zolty, zielony, Szachownica
+from gra import Szachownica
+from warcaby_kolory import kolory
 from warcaby_gra import Gra
 
 
@@ -11,8 +12,12 @@ class Menu():
 
     def rysuj_tlo(self, win):
         boazeria = pygame.image.load("boazeria.jpg")
+        siding = pygame.image.load("siding.jpg")
+
         pygame.transform.scale(boazeria, (self.szerokosc, self.dlugosc))
         win.blit(boazeria, (self.dlugosc, 0))
+
+        win.blit(siding, (self.dlugosc, 700))
 
     def zawartosc(self, win):
         '''Funkcja, która tworzy zawartość menu'''
@@ -23,26 +28,36 @@ class Menu():
         # Tekst powitalny
         pygame.font.init()
         czcionka_wstep = pygame.font.SysFont("Algerian", 36)
-        wstep = czcionka_wstep.render("Zapraszam do gry!", False, zolty)
+        wstep = czcionka_wstep.render("Zapraszam do gry!", False, kolory["żółty"])
         win.blit(wstep, (820, 50))
 
         # Informacje o kolejce
         czcionka_kolejka = pygame.font.SysFont("Times New Roman", 24)
-        kolejka = czcionka_kolejka.render("Aktualna kolejka: ", False, bialy)
-        win.blit(kolejka, (820, 150))
-        pygame.draw.rect(win, czarny, (1030, 145, 45, 45))
-        pygame.draw.rect(win, gra.kolejka, (1030, 145, 40, 40))
+        kolejka = czcionka_kolejka.render("Aktualna kolejka: ", False, kolory["biały"])
+        win.blit(kolejka, (820, 250))
+        pygame.draw.rect(win, kolory["czarny"], (1030, 245, 45, 45))
+        pygame.draw.rect(win, gra.kolejka, (1030, 245, 40, 40))
 
         # Informacje o liczbie pozostąłych krążków
         czcionka_lkrazek = pygame.font.SysFont("Times New Roman", 18)
-        krazek_b = czcionka_lkrazek.render(f"Liczba pozostałych białych krążków:", False, zielony)
-        krazek_c = czcionka_lkrazek.render(f"Liczba pozostałych czerwonych krążków:", False, zielony)
-        lkrazek_b = czcionka_lkrazek.render(f"{szachownica.liczba_bialych}", False, bialy)
-        lkrazek_c = czcionka_lkrazek.render(f"{szachownica.liczba_czerwonych}", False, czerwony)
-        win.blit(krazek_b, (820, 250))
-        win.blit(krazek_c, (820, 300))
-        win.blit(lkrazek_b, (1150, 250))
-        win.blit(lkrazek_c, (1150, 300))
+        krazek_b = czcionka_lkrazek.render(f"Liczba pozostałych białych krążków:", False, kolory["zielony"])
+        krazek_c = czcionka_lkrazek.render(f"Liczba pozostałych czerwonych krążków:", False, kolory["zielony"])
+        lkrazek_b = czcionka_lkrazek.render(f"{szachownica.liczba_bialych}", False, kolory["biały"])
+        lkrazek_c = czcionka_lkrazek.render(f"{szachownica.liczba_czerwonych}", False, kolory["czerwony"])
+        win.blit(krazek_b, (820, 375))
+        win.blit(krazek_c, (820, 425))
+        win.blit(lkrazek_b, (1150, 375))
+        win.blit(lkrazek_c, (1150, 425))
 
+    def przycisk(self, win):
+        pygame.font.init()
 
+        przycisk_x = 900
+        przycisk_y = 735
+        przycisk_dl = 35
+        przycisk_szer = 215
 
+        pygame.draw.rect(win, kolory["szary"], (przycisk_x, przycisk_y, przycisk_szer, przycisk_dl))
+        czcionka_przycisk = pygame.font.SysFont("Arial", 14)
+        tekst_przycisk = czcionka_przycisk.render("Jeżeli chcesz zakończyć grę, kliknij tutaj!", False, kolory["czarny"])
+        win.blit(tekst_przycisk, (przycisk_x , przycisk_y + (przycisk_dl)//4))
