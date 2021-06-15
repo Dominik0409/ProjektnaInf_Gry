3fe263864b4f2ecc74632ae0f7cb5ee0e6503648
rizun1999 <62021379+rizun1999@users.noreply.github.com>
2021-06-15 11:09:36 +0200

    Update gra1.py

--- gra1.py
+++ gra1.py
@@ -11,45 +11,62 @@ pole_rozmiar = szach_szer//wiersze
 class Szachownica():
 
     def __init__(self):
-        self.szachownica = np.zeros((8,8))
+        self.szachownica_uklad = np.zeros((8,8)) # Ten atrybut tworzy macierz o wymiarach 8x8 reprezentująca szachownicę; wszystkie zera reprezentują brak krązka na danym polu na szachownicy
         self.liczba_czerwonych = 12
         self.liczba_bialych = 12
+        self.kolejka = kolory["czerwony"] # Gra zaczyna się od gracza czerwonego
         self.rozstawienie(win)
 
     def draw(self, win):
+        '''Funkcja służąca do narysowania typowej planszy do warcab'''
+        
         win.fill(kolory["czarny"])
         for w in range(wiersze):
             for k in range(w%2, wiersze, 2):
                 pygame.draw.rect(win, kolory["czerwony"], (pole_rozmiar*w,pole_rozmiar*k,pole_rozmiar,pole_rozmiar))
 
     def rozstawienie(self, win):
+        '''Jest to funkcja, która aktualizuje macierz repezentującą szachownicę'''
+        
+        # Dla wierszy od 0 do 2 (w bibliotece pygame początek układu współrzędnych ekranu jest zawsze w lewym górnym rogu) elementy macierzy będą równe 1
+        # Dla wierszy od 5 do 7 elementy macierzy będą równe 2
+        
         for w in range(wiersze):
             for k in range(kolumny):
                 if w < 3:
-                    if k%2 == (w+1)%2:
-                        self.szachownica[w,k] = 1
+                    if k % 2 == (w + 1) % 2: # Ten warunek pozwala na rozmieszczenie krążków "zygzakowo"
+                        self.szachownica_uklad[w,k] = 1
                 elif w > 4:
                     if k % 2 == (w + 1) % 2:
-                        self.szachownica[w,k] = 2
+                        self.szachownica_uklad[w,k] = 2
 
     def rozstawienie_rysuj(self, win):
+        ''' Funkcja, za pomocą której program rysuje obiekty klasy Krazek() na wyznaczonych polach planszowych'''
+        
         for w in range(wiersze):
             for k in range(kolumny):
-                if self.szachownica[w,k] == 1:
+                if self.szachownica_uklad[w,k] == 1: # Dla elementów macierzy reprezentującej szachownicę równych 1, program rysuje białe krążki
                     krazek_bialy = Krazek(w, k, kolory["bialy"])
                     krazek_bialy.draw(win)
-                elif self.szachownica[w,k] == 2:
+                elif self.szachownica_uklad[w,k] == 2: # Dla elementów macierzy reprezentującej szachownicę równych 2, program rysuje czerwone krążki
                     krazek_czerwony = Krazek(w, k, kolory["czerwony"])
                     krazek_czerwony.draw(win)
 
     def ruch_krazek(self, krazek, wiersz, kolumna):
-        print(self.szachownica)
-        self.szachownica[krazek.wiersz, krazek.kolumna], self.szachownica[wiersz,kolumna] = self.szachownica[wiersz,kolumna], self.szachownica[krazek.wiersz, krazek.kolumna]
+        '''Kiedy krążek się porusza, pozycje w macierzy reprezntującej szachownicę zamieniając się miejscami'''
+        
+        self.szachownica_uklad[krazek.wiersz, krazek.kolumna], self.szachownica_uklad[wiersz,kolumna] = self.szachownica_uklad[wiersz,kolumna], self.szachownica_uklad[krazek.wiersz, krazek.kolumna]
         krazek.ruch(wiersz,kolumna)
-        pass
 
     def get_krazek(self, wiersz, kolumna):
-        return self.szachownica[wiersz,kolumna]
+        '''Funkcja pozwalająca na zwrócenie obiektu klasy Krazek()'''
+        
+        if self.szachownica_uklad[wiersz,kolumna] == 1:
+            kolor = kolory["biały"]
+            return Krazek(wiersz, kolumna, kolor)
+        elif self.szachownica_uklad[wiersz,kolumna] == 2:
+            kolor = kolory["czerwony"]
+            return Krazek(wiersz, kolumna, kolor)
 
 class Krazek():
 
@@ -59,17 +76,23 @@ class Krazek():
         self.kolumna = kolumna
         self.x = 0
         self.y = 0
-        self.pozycja()
+        self.pozycja() # W każdym przypadku zmiany pozycji danego krążka, ta funkcja jest wywoływana
 
     def pozycja(self):
+        '''Funkcja wspomagająca obliczenie pozycji danego krążka w układzie kartezjańskim'''
+        
         self.x = win_szer/2 + self.kolumna*pole_rozmiar
         self.y = win_dl/2 * self.wiersz*pole_rozmiar
 
     def draw(self, win):
+        '''Funkcja rysująca krążek o danych współrzędnych i kolorze'''
+        
         promien = pole_rozmiar/3
         pygame.draw.circle(win, self.kolor, (self.x, self.y), promien)
         
     def ruch(self, wiersz, kolumna):
+        '''Ta funkcja jest wywoływana, kiedy dany krążek się porusza po planszy'''
+        
         self.wiersz = wiersz
         self.kolumna = kolumna
         self.pozycja()
