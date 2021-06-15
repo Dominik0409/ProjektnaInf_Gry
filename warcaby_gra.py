f77709b159c495c0823678d056372f6e92e2ad63
rizun1999 <62021379+rizun1999@users.noreply.github.com>
2021-06-15 10:27:10 +0200

    Update warcaby_gra.py

--- warcaby_gra.py
+++ warcaby_gra.py
@@ -1,4 +1,4 @@
-''' Ten plik służy optymalizacji kodu '''
+''' Ten plik służy do optymalizacji kodu '''
 
 import pygame
 from gra1 import Szachownica, Krazek
@@ -24,3 +24,51 @@ class Gra():
         self.szachownica.rozstawienie(self.win)
         self.szachownica.rozstawienie_rysuj(self.win)
 
+    def update(self):
+        self.szachownica.rozstawienie_rysuj(self.win)
+        pygame.display.update()
+
+    def select(self, wiersz, kolumna):
+        '''Wywołanie tej funkcji spowoduje zmianę atrybutu self.selected na True, jeśli użytkownik kliknie na obszar zawierający obiekt klasy Krazek()'''
+        
+        krazek = self.szachownica.get_krazek(wiersz, kolumna)
+        if self.selected: # Jeżeli użytkownik po wybraniu krążka wybierze pole, wtedy można przesunąć krążek na wybrane pole
+            wynik = self.ruch(wiersz,kolumna)
+            if not wynik: # Kiedy wykonanie ruchu jest niemożliwe, program pozwala użytkownikowi wybrać pole jeszcze raz
+                self.selected = None
+                self.select(wiersz,kolumna)
+
+        if krazek != None and krazek.kolor == self.szachownica.kolejka: # Warunek określający stan, w którym użytkownikwybierze krążek o kolorze obobwiązującej kolejki
+            self.selected = krazek
+            pygame.draw.circle(self.win, kolory["szary"], (krazek.x, krazek.y), 20)
+            return True
+
+        return False
+
+    def ruch(self, wiersz, kolumna):
+        '''Funkcja umożliwiająca ruch krążków po planszy'''
+        
+        krazek = self.szachownica.get_krazek(wiersz, kolumna)
+        if self.selected and krazek == None: # Jeżeli jakis krążek jest wybrany (self.selected = True) oraz wybrane pole do przemiezszczenia się jest puste, wtedy wywołuje się funkcja przemiszczająca krążek, zdefiniowana w klasie Szachownica()
+            self.szachownica.ruch_krazek(self.selected, wiersz, kolumna)
+            self.szachownica.rozstawienie_rysuj(self.win)
+            self.zmiana_kolejki()
+        else:
+            return False
+
+        return True
+
+    def zbij(self, wiersz, kolumna):
+            if self.szachownica.szachownica_uklad[wiersz, kolumna] == 1:
+                self.szachownica.szachownica_uklad[wiersz, kolumna] == 2
+            else:
+                self.szachownica.szachownica_uklad[wiersz, kolumna] == 1
+
+    def zmiana_kolejki(self):
+        '''Po wykonaniu ruchu przez krążek konkretnego koloru, możliwe jest wybieranie krążków koloru przeciwnego'''
+        
+        if self.szachownica.kolejka == kolory["czerwony"]:
+            self.szachownica.kolejka = kolory["biały"]
+        else:
+            self.szachownica.kolejka = kolory["czerwony"]
+        pygame.draw.rect(self.win, self.szachownica.kolejka, (1030, 245, 40, 40)) # Prostokąt w części zawierającej menu pozwala określić, czyja jest kolejka
